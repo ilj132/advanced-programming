6986a46ec3f8331c4d9a285d68f2fe93615c24f5
Isaac de Lyra Jr <isaac.lyra.junior@gmail.com>
2020-06-14 20:23:50 -0300

    question 4 on the structured list

--- listas/lista estruturadas/questao4est.cpp
+++ listas/lista estruturadas/questao4est.cpp
@@ -1,6 +1,7 @@
 /*Qual o resultado da execução do seguinte programa, supondo que o usuário digite os valores 3 e 4? De uma maneira geral, o que faz este programa? 
 
-Ao substituir o tipo da função main para 'int', a função permite multiplicar os dois numeros digitados.
+Ao substituir o tipo da função main para 'int', o programa permite multiplicar os dois numeros digitados.
+
 */
 #include <iostream>
 using namespace std;
