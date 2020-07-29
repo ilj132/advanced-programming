77f01414562355e64d554634f74f6fe45c4d74a8
Isaac de Lyra Junior <isaac.lyra.junior@gmail.com>
2020-07-29 11:30:19 -0300

    starting a resolution of problem 1

---
+++ listas/classes/listaclass1.cpp
@@ -0,0 +1,35 @@
+/*Desenvolva classes que possam representar pontos e retângulos no espaço 2D, de
+acordo com as seguintes especificações:
+ Defina o tipo Ponto para representar um ponto em coordenadas cartesianas.
+ Crie uma função imprimir, capaz de imprimir um Ponto na tela.
+ Sobrecarregue o operador << usando a função imprimir.
+ Forneça uma implementação para uma função distancia que calcula a distância
+Euclidiana entre dois dados do tipo Ponto.
+ Defina um tipo Retangulo para representar retângulos que são paralelos aos ei-
+xos no sistema de coordenadas cartesianas. Represente um retângulo pelos seus
+pontos superior direito (ponto de coordenadas máximas) e inferior esquerdo (co-
+ordenadas mínimas), usando o tipo Ponto.
+ Escreva a função area que calcula a área de um Retangulo.
+ Escreva uma função pontoInterno que retorna o resultado do teste (true ou fal-
+se) sobre se um Ponto está ou não dentro de um Retangulo.
+ Sobrecarregue o operador < usando a função pontoInterno.*/
+
+#include <iostream>
+
+using namespace std;
+
+class Ponto
+{
+private:
+public:
+};
+
+class Retangulo
+{
+private:
+public:
+};
+
+int main()
+{
+}
